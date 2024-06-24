d914f7fb1eddf892e7287b653070f28e790cbb42
H1cham1 <hichamt312@gmail.com>
2024-06-24 22:36:43 +0200

    Fixed error code not running

--- apotheek/apotheek/settings.py
+++ apotheek/apotheek/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-"emmet.includeLanguages": {"django-html": "html",} # type: ignore
+# "emmet.includeLanguages": {"django-html": "html",} # type: ignore
