d42094dc8f54faa590148e355206eec3a8781768
H1cham1 <hichamt312@gmail.com>
2024-06-26 19:48:19 +0200

    added views, login and register

--- apotheek/base/views.py
+++ apotheek/base/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "base/index.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log the user in and redirect to index
+            login(request, user)
+            return redirect("index")
+
+    else:
+        form = UserCreationForm()
+
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
