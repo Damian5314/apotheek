42b0c9539a68327a369703b94ebdc102faadf55a
Damian5314 <damianwillmse@gmail.com>
2024-06-26 19:50:49 +0200

    Add beginnings for models.py

--- apotheek/base/models.py
+++ apotheek/base/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    #User = models.OneToOneField(User, on_delete=models.CASCADE)
+    BioText = models.TextField()
+    City = models.CharField(max_length=100)
+    DateOfBirth = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.User.username
+    
+#class Medicine(models.Model):
+    #Name = 
+    #Manufacturer = 
+    #Cures = 
+    #SideEffects = 
+
+#class Collection(models.Model):
+    #Medicine = 
+    #User = 
+    #Date = 
+    #Collected = 
+    #CollectedApproved = 
+    #CollectedApprovedBy = 
