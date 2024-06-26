07fa8ed9e55eb1c5691a829e0f905b839403f71f
H1cham1 <hichamt312@gmail.com>
2024-06-26 20:46:33 +0200

    made profile

--- apotheek/base/urls.py
+++ apotheek/base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", include('django.contrib.auth.urls')),
     path("register/", views.register, name='register'),
     path('logout_user/', views.logout_user, name="logout_user"),
+    path('profile/', views.profile, name='profile'),
 ]
