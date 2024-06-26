9d2dcd8473397f506bbcd1192b48459bc09d0421
H1cham1 <hichamt312@gmail.com>
2024-06-26 19:52:20 +0200

    Actually added login and register now

--- apotheek/base/urls.py
+++ apotheek/base/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("", include('django.contrib.auth.urls')),
+    path("register/", views.register, name='register'),
+    path('logout_user/', views.logout_user, name="logout_user"),
 ]
