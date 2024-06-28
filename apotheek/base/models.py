472ea545786dbe307bbecb80c2a8461d9e7c64c4
Damian5314 <damianwillmse@gmail.com>
2024-06-28 15:47:32 +0200

    Add everything to models

--- apotheek/base/models.py
+++ apotheek/base/models.py
@@ -7,9 +7,9 @@ from django.dispatch import receiver
 # Create your models here.
 class Profile(models.Model):
     User = models.OneToOneField(User, on_delete=models.CASCADE)
-    BioText = models.TextField()
+    BioText = models.TextField(blank=True)
     City = models.CharField(max_length=100)
-    DateOfBirth = models.DateField(null=True, blank=True)
+    DateOfBirth = models.DateField()
 
     def __str__(self):
         return self.User.username
@@ -21,15 +21,21 @@ def create_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 class Medicine(models.Model):
-    Name = models.CharField(max_length=30, blank=True)
-    Manufacturer = models.CharField(max_length=30, blank=True)
+    Name = models.CharField(max_length=100, blank=True)
+    Manufacturer = models.CharField(max_length=100, blank=True)
     Cures = models.CharField(max_length=30, blank=True)
     SideEffects = models.CharField(max_length=30, blank=True)
 
+    def __str__(self):
+        return self.Name
+
 class Collection(models.Model):
-    Medicine = models.CharField(max_length=30, blank=True)
+    Medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
     User = models.ForeignKey(User, on_delete=models.CASCADE)
-    # Date =
-    # Collected =
+    Date = models.DateField()
+    Collected = models.BooleanField(default=False)
     CollectedApproved = models.BooleanField(default=False)
-    #CollectedApprovedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='approved_by', null=True, blank=True)
+    CollectedApprovedBy = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='approved_collections')
+
+    def __str__(self):
+        return f"{self.User.username} - {self.Medicine.Name}"
